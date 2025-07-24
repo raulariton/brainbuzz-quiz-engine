9830aae62b46e30d8b762cdf465e8ac4de6100b4
dennismanoila <dennis.manoila05@e-uvt.ro>
2025-07-24 16:18:20 +0300

    Add POST /results endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import testRoute from './routes/testRoute.js';
+import resultsRoute from './routes/resultRoute.js'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
 app.use('/', testRoute);
+app.use('/', resultsRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
