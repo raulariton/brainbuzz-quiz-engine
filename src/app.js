81eaafdb6813430ad3ee16b8cef07dbc3cb30656
Oiaga Lucas <lucas.oiaga@vspartners.us>
2025-07-25 00:27:11 +0300

    Generate Quizz For Type
    Promptul este numa de testare
    Testat si raspunde ollama(doar in postman)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import testRoute from './routes/testRoute.js';
+import dotenv from 'dotenv';
+//import testRoute from './routes/testRoute.js';
+import { QuizController } from './controllers/quizController.js';
+
+//codu asta exista pentru a incarca variabilele din fisierul .env din root folder
+dotenv.config({ path: '../.env' });
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use('/', testRoute);
+//app.use('/', testRoute);
+app.use('/quiz', QuizController.handleQuizRequest);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Primul test on ${port}`);
 });
