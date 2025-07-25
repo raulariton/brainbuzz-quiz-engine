c74936cb432f22fdb93dcfdaf11e778a877d1c60
raul-it0o0 <ariton.raul@gmail.com>
2025-07-25 12:38:29 +0300

    Remove testRoute and testController

--- src/routes/testRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { TestController } from '../controllers/testController.js';
-
-const router = Router();
-router.get('/', TestController.getTest);
-
-export default router;
