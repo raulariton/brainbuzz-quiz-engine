613455454323fa6a36ecb52f738ccdcb5181cc42
raul-it0o0 <ariton.raul@gmail.com>
2025-08-06 16:20:10 +0300

    Update `GET /quiz` route method to also return a URL to an image that matches the quiz type.
    Additionally:
    - Added a method `generateQuizImage()` that makes the request for the image generation, using only the quiz type
    - Updated `quizTypes` to also include a prompt used to create an image that matches with the respective quiz type.

--- src/services/imageGenerationServices.js
+++ src/services/imageGenerationServices.js
@@ -8,22 +8,18 @@ fal.config({
 });
 
 
-const GENERATION_PROMPT = `Using the provided picture of a profile picture of a human,
+const REWARD_IMAGE_GENERATION_PROMPT = `Using the provided picture of a profile picture of a human,
 generate an image of the human as a king sitting on a throne.
 If the image is not a human, create a poster image with the text: {{DISPLAY_NAME}}. 
 `;
 
 export const generateRewardImage = async ({ imageUrl, userDisplayName }) => {
   try {
-    // console.log("Apelez fal.subscribe cu:");
-    // console.log("➡️ imageUrl:", imageUrl);
-    // console.log("➡️ userDisplayName:", userDisplayName);
-
     const result = await fal.subscribe(
       'fal-ai/flux-kontext/dev',
       {
         input: {
-          prompt: GENERATION_PROMPT.replace('{{DISPLAY_NAME}}', userDisplayName),
+          prompt: REWARD_IMAGE_GENERATION_PROMPT.replace('{{DISPLAY_NAME}}', userDisplayName),
           image_url: imageUrl
         },
         logs: true,
@@ -38,11 +34,39 @@ export const generateRewardImage = async ({ imageUrl, userDisplayName }) => {
     // return generated image URL
     //console.log("URL imagine generată:", result.data.images[0].url);
     return result.data.images[0].url;
-    
+
 
 
   } catch (error) {
-    console.error('Error generating image:', error);
-    throw new Error('Failed to generate image');
+    console.error('Error generating reward image:', error);
+    throw new Error('Failed to generate reward image');
   }
 };
+
+export async function generateQuizImage({imagePrompt}) {
+  try {
+    const result = await fal.subscribe(
+      "fal-ai/flux-1/schnell",
+      {
+        input: {
+          num_inference_steps: 6, // default is 4
+          prompt: imagePrompt,
+          image_size: "square",
+        },
+        logs: true,
+        onQueueUpdate: (update) => {
+          if (update.status === "IN_PROGRESS") {
+            update.logs.map((log) => log.message).forEach(console.log);
+          }
+        }
+      }
+    );
+
+    // return generated image URL
+    return result.data.images[0].url;
+
+  } catch (error) {
+    console.error('Error generating quiz image:', error);
+    throw new Error('Failed to generate quiz image');
+  }
+}
